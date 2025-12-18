ac3572a7f6b8362e60791c81ee28be595daae456
merin-22 <valepa221203@gmail.com>
2025-12-18 20:07:07 +0300

    Fix Player Assignment to Enemy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    //public Player player;
-    public GameObject player;
+    public PlayerStateMachine player;
+
     private void Awake()
     {
         instance = this;
-        player = GameObject.FindGameObjectWithTag("Player");
     }
 
 }
