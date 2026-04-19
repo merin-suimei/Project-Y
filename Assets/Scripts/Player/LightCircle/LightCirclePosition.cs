41a6806a712eb372f7b2739598930ae15a957830
Anastasia Prokop <gpae98@gmail.com>
2026-04-19 19:23:31 +0300

    add shadow under player

--- Assets/Scripts/Player/LightCircle/LightCirclePosition.cs
+++ Assets/Scripts/Player/LightCircle/LightCirclePosition.cs
@@ -7,6 +7,9 @@ public class LightCirclePosition : MonoBehaviour
     [SerializeField] private float _emissionIntensity;
     [SerializeField] private float _maxBrightness;
     [SerializeField] private float _circleSoftness;
+    [SerializeField] private float _circleShadowRadius;
+    [SerializeField] private float _circleShadowSoftness;
+    [SerializeField] private Vector2 _circleShadowOffset;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,10 +29,14 @@ public class LightCirclePosition : MonoBehaviour
         }
 
         var p = Player.position;
+        Vector3 _vector3ShadowOffset = new Vector3(_circleShadowOffset.x, 0.0f, _circleShadowOffset.y);
         Shader.SetGlobalVector("_PositionPlayerLight", new Vector4(p.x, p.y, p.z, 1f));
         Shader.SetGlobalFloat("_LightCircleRadius", _circleRadius);
         Shader.SetGlobalFloat("_LightCircleIntensity", _emissionIntensity);
         Shader.SetGlobalFloat("_LightCircleMaxBrightness", _maxBrightness);
         Shader.SetGlobalFloat("_LightCircleSoftness", _circleSoftness);
+        Shader.SetGlobalFloat("_LightCircleShadowRadius", _circleShadowRadius);
+        Shader.SetGlobalFloat("_LightCircleShadowSoftness", _circleShadowSoftness);
+        Shader.SetGlobalVector("_LightCircleShadowOffset", _vector3ShadowOffset);
     }
 }
