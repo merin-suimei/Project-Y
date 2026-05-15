efc0421f72241c1e0feea7101bdfaba6e8cede0b
merin-22 <valepa221203@gmail.com>
2026-05-15 21:54:27 +0300

    Add Settings Update/Reset

--- Assets/Scripts/GameInitiator.cs
+++ Assets/Scripts/GameInitiator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public static class GameInitiator
 {
@@ -19,15 +20,13 @@ public static class GameInitiator
         EventBus.Subscribe(EventType.ResetGameState, ResetGameState);
         EventBus.Subscribe(EventType.UpdateGameState, UpdateGameState);
         EventBus.Subscribe(EventType.ResetSettings, ResetSettings);
-        EventBus.Subscribe(EventType.UpdateSettings, UpdateSettings);
+        EventBus.Subscribe<List<SettingsPair>>(EventType.UpdateSettings, UpdateSettings);
     }
 
     private static void ResetGameState()
     {
-        //TODO
         currentGameState.currentLevel = defaultGameState.currentLevel;
         currentGameState.isHardMode = defaultGameState.isHardMode;
-
     }
 
     private static void UpdateGameState()
@@ -37,11 +36,56 @@ public static class GameInitiator
 
     private static void ResetSettings()
     {
-        //TODO
+        currentSettings.screenResolution = new Vector2Int(Screen.currentResolution.width, Screen.currentResolution.height);
+        currentSettings.fullScreen = defaultSettings.fullScreen;
+        currentSettings.VSync = defaultSettings.VSync;
+
+        currentSettings.soundVolume = defaultSettings.soundVolume;
+        currentSettings.musicVolume = defaultSettings.musicVolume;
+        currentSettings.effectsVolume = defaultSettings.effectsVolume;
+
+        ApplySettings();
     }
 
-    private static void UpdateSettings()
+    private static void UpdateSettings(List<SettingsPair> list)
     {
-        //TODO
+        foreach (SettingsPair pair in list)
+        {
+            switch (pair.Name)
+            {
+                case "screenResolution":
+                    currentSettings.screenResolution = pair.Value.vector2IntValue;
+                    break;
+                case "fullScreen":
+                    currentSettings.fullScreen = pair.Value.boolValue;
+                    break;
+                case "VSync":
+                    currentSettings.VSync = pair.Value.boolValue;
+                    break;
+                case "soundVolume":
+                    currentSettings.soundVolume = pair.Value.floatValue;
+                    break;
+                case "musicVolume":
+                    currentSettings.musicVolume = pair.Value.floatValue;
+                    break;
+                case "effectsVolume":
+                    currentSettings.effectsVolume = pair.Value.floatValue;
+                    break;
+
+                default:
+                    Debug.LogError("Unknown setting name: " + pair.Name);
+                    break;
+            }
+        }
+
+        ApplySettings();
+    }
+
+    private static void ApplySettings()
+    {
+        Screen.SetResolution(currentSettings.screenResolution.x, currentSettings.screenResolution.y, currentSettings.fullScreen);
+        QualitySettings.vSyncCount = currentSettings.VSync ? 1 : 0;
+
+        // TODO: Double check audio settings application
     }
 }
